909dfa46e2b2ea53305dc0ab50a10dc9aeb31bba
Sudarsan Sahoo <sudarsan.sahoo@ericsson.com>
2016-09-28 19:58:14 +0530

    Added for env variable test

--- test.js
+++ test.js
@@ -7,3 +7,5 @@ var connect = mongoose.connect('mongodb://localhost:27017/iotaccdb');
 connect.then(function(){
     console.log('connected');
 })
+
+console.log("Ennnnnnnnnnnnnnnnnnn"+process.env.NODE_ENV)
